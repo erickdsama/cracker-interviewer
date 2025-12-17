e7e9e5b8009e03e885f946adc01fd0ec909fa3df
erick <erickdsama@gmail.com>
2025-12-17 16:08:07 -0700

    fix vuln

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000', // TODO: Use env var
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json',
     },
